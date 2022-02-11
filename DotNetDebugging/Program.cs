3f0cd758060501bd21aa0d23f6461ff27e062044
王珏 <jue952@ecjtu.edu.cn>
2022-02-11 16:05:18 +0800

    .NET代码调试

---
+++ DotNetDebugging/Program.cs
@@ -0,0 +1,17 @@
+﻿int result = Fibonacci(5);
+System.Console.WriteLine(result);
+
+static int Fibonacci(int n)
+{
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+    return n == 0 ? n1 : n2;
+}
