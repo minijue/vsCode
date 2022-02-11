335bb7a04d942975f980162af44f175e9883eb22
王珏 <jue952@ecjtu.edu.cn>
2022-02-11 16:23:03 +0800

    日志记录和跟踪

--- DotNetDebugging/Program.cs
+++ DotNetDebugging/Program.cs
@@ -1,8 +1,12 @@
-﻿int result = Fibonacci(5);
+﻿using System.Diagnostics;
+
+int result = Fibonacci(5);
 System.Console.WriteLine(result);
 
 static int Fibonacci(int n)
 {
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}the number");
     int n1 = 0;
     int n2 = 1;
     int sum;
@@ -12,6 +16,8 @@ static int Fibonacci(int n)
         sum = n1 + n2;
         n1 = n2;
         n2 = sum;
+        Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
     }
+    Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
     return n == 0 ? n1 : n2;
 }
