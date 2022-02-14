3ae54e46e821dc98fb36653fe295820b7192ea8c
王珏 <jue952@ecjtu.edu.cn>
2022-02-14 18:26:46 +0800

    Java备课用项目

---
+++ JavaDemo/JAVADEMO.java
@@ -0,0 +1,23 @@
+
+public class JAVADEMO {
+    public static void main(String[] args) {
+        int x = 1;
+        switch (x) {
+            case 0:
+                System.out.println("x is less than one");
+                break;
+            case 1:
+                System.out.println("x is less than two");
+                break;
+            case 2:
+                System.out.println("x is less than three");
+                break;
+            case 3:
+                System.out.println("x is less than four");
+                break;
+            case 4:
+                System.out.println("x is less than five");
+                break;
+        }
+    }
+}
