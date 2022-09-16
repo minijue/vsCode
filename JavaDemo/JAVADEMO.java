e03be67d510bf80826b1cab4b73da06902ba832c
wangjue <jue952@126.com>
2022-09-16 17:20:43 +0800

    add new files

--- JavaDemo/JAVADEMO.java
+++ JavaDemo/JAVADEMO.java
@@ -1,18 +1,21 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class JAVADEMO {
     public static void main(String[] args) {
-        Scanner sin = new Scanner(System.in);
-        String article = sin.nextLine();
-        sin.close();
-        String[] words = article.split(" ");
-        for (int i = 0; i < words.length; i++) {
-            StringBuffer sb = new StringBuffer(words[i]);
-            System.out.print(sb.reverse());
-            if (i < words.length - 1)
-                System.out.print(" ");
-            else
-                System.out.println();
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new FileInputStream("d:\\a.txt"));
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        String str;
+        while (sc.hasNext()) {
+            str = sc.nextLine();
+            System.out.println(str);
+        }
+        sc.close();
+
     }
 }
