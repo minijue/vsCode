8ff9eb88714033d26315e4eb3a1076cac80b9948
wangjue <jue952@126.com>
2022-12-02 10:13:25 +0800

    insert\update\delete examples

--- MybatisDemo/mybatisdemo/src/com/example/wangjue/App.java
+++ MybatisDemo/mybatisdemo/src/com/example/wangjue/App.java
@@ -2,6 +2,7 @@ package com.example.wangjue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
@@ -21,11 +22,39 @@ public class App {
 
         SqlSessionFactory build = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession sqlSession = build.openSession();
+        
+        // --insert into--
+        // Dog dog = new Dog();
+        // dog.setName("Haha");
+        // dog.setAge(6);
+        // sqlSession.insert("dog.insertDog", dog);
 
+        // HashMap<String, Object>  mapdog = new HashMap<>();
+        // mapdog.put("name", "hehe");
+        // mapdog.put("age", 4);
+        // sqlSession.insert("dog.insertDog", mapdog);
+
+        // --update--
+        // HashMap<String, Integer> mapage = new HashMap<>();
+        // mapage.put("age", 10);
+        // sqlSession.update("dog.updateDog", 11);
+
+        // --delete--
+        // sqlSession.delete("dog.deleteDog", 2);
+
+        sqlSession.commit();
+
+        // query
         List<Object> objects = sqlSession.selectList("dog.selectDog");
 
         for (Object ob : objects) {
             System.out.println((Dog) ob);
+        }
+
+        System.out.println();
+        List<Dog> dogs = sqlSession.selectList("dog.selectByName", "Haha");
+        for (Dog d : dogs) {
+            System.out.println(d);
         }
         
         sqlSession.close();
