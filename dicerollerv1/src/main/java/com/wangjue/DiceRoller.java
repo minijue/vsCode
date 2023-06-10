f66abb148f67aec7f7f6d55a010c66dede09b7e6
wangjue <wangjue@wjzl.asuscomm.com>
2023-06-10 19:04:35 +0800

    新建JSP项目，EL调用静态方法

---
+++ dicerollerv1/src/main/java/com/wangjue/DiceRoller.java
@@ -0,0 +1,7 @@
+package com.wangjue;
+
+public class DiceRoller {
+    public static int rollDice() {
+        return (int) ((Math.random()*6)+1);
+    }
+}
---
+++ dicerollerv1/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ taglib prefix="mine" uri="DiceFunctions" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+<p>${mine:rollIt()}</p>
+</body>
+</html>
